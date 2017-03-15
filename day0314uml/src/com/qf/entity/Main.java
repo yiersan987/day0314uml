d9c83662400da9506e4df95fecb0210a9ee04625
Administrator <Administrator@PC-20161213IGRO>
2017-03-15 11:45:26 +0800

    第一次

---
+++ day0314uml/src/com/qf/entity/Main.java
@@ -0,0 +1,12 @@
+package com.qf.entity;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("svn");
+	}
+	public static void sum(int x,int y){
+	   int sum;
+	   
+	   sum = x + y ;
+	} 
+}
