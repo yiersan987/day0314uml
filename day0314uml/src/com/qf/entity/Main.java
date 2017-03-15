d3f069e6e97fe8d3203c70c37202df9a48b6f4dc
Administrator <Administrator@PC-20161213IGRO>
2017-03-15 11:46:38 +0800

    add()

--- day0314uml/src/com/qf/entity/Main.java
+++ day0314uml/src/com/qf/entity/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("svn");
 	}
+	public static void add(){
+		
+	}
 	public static void sum(int x,int y){
 	   int sum;
 	   
